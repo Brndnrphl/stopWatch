f8556b5845bdf9fe30742afa703f77a2252f07c9
Brndn <brndnrphl@gmail.com>
2023-11-07 00:16:08 +0700

    tidied things up 	modified:   script.js

--- script.js
+++ script.js
@@ -1,54 +1,51 @@
 let interval;
-let hour = "00";
-let minute = "00";
-let second = "00";
-let milliSecond = "00";
-let isRunning = false;
-let time = document.querySelector(".time");
-let start = document.querySelector(".start");
-let stop = document.querySelector(".stop");
-let reset = document.querySelector(".reset");
-time.innerHTML = `${hour}:${minute}:${second}<span class="smaller">.${milliSecond}</span>`;
+let hours = "00";
+let minutes = "00";
+let seconds = "00";
+let milliseconds = "00";
+const time = document.querySelector(".time");
+const start = document.querySelector(".start");
+const stop = document.querySelector(".stop");
+const reset = document.querySelector(".reset");
+
+time.innerHTML = `${hours}:${minutes}:${seconds}<span class="smaller">.${milliseconds}</span>`;
 
 start.addEventListener("click", () => {
-  if (!isRunning) {
-    isRunning = true;
+  if (!interval) {
     interval = setInterval(() => {
-      milliSecond++;
-      if (milliSecond == 100) {
-        second++;
-        milliSecond = 0;
+      milliseconds++;
+      if (milliseconds == 100) {
+        seconds++;
+        milliseconds = 0;
       }
-      if (second == 60) {
-        minute++;
-        second = 0;
+      if (seconds == 60) {
+        minutes++;
+        seconds = 0;
       }
-      if (minute == 60) {
-        hour++;
-        minute = 0;
+      if (minutes == 60) {
+        hours++;
+        minutes = 0;
       }
-      if (hour == 24) {
-        hour = 0;
+      if (hours == 24) {
+        hours = 0;
       }
-      time.innerHTML = `${String(hour).padStart(2, "0")}:${String(
-        minute
-      ).padStart(2, "0")}:${String(second).padStart(
+      time.innerHTML = `${String(hours).padStart(2, "0")}:${String(
+        minutes
+      ).padStart(2, "0")}:${String(seconds).padStart(
         2,
         "0"
-      )}<span class="smaller">.${String(milliSecond).padStart(2, "0")}</span>`;
+      )}<span class="smaller">.${String(milliseconds).padStart(2, "0")}</span>`;
     }, 10);
   }
 });
 
 stop.addEventListener("click", () => {
   clearInterval(interval);
-  isRunning = false;
+  interval = null;
 });
 
 reset.addEventListener("click", () => {
-  if (interval) {
-    clearInterval(interval);
-    isRunning = false;
-    time.innerHTML = `00:00:00<span class="smaller">.00</span>`;
-  }
+  clearInterval(interval);
+  interval = null;
+  time.innerHTML = `00:00:00<span class="smaller">.00</span>`;
 });
